9b1d91fa50d4603d484ab80fdf402abbab5f70fa
Marc Kohli <marckohli@gmail.com>
2007-01-29 20:42:29 +0000

    Image resizing working.  A few bug fixes.
    git-svn-id: http://referralpacs.googlecode.com/svn/v0.2@92 927d8226-84e8-11dd-ad27-0f66e57d2035

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -52,3 +52,45 @@ Ajax.Responders.register({
     
     }
 });
+
+// Code for photo resize sliders
+
+var scalePhotos, demoSlider, maxHeight;
+
+function scaleIt(v) {
+    if(scalePhotos == null){scalePhotos = document.getElementsByClassName('scale-image');}
+    
+      floorSize = .226;
+      ceilingSize = 1.0;
+      v = floorSize + (v * (ceilingSize - floorSize));
+    
+      for (i=0; i < scalePhotos.length; i++) {
+    	scalePhotos[i].style.width = (v*1200)+'px';
+      }
+}
+
+
+function init() {
+
+    demoSlider = new Control.Slider('slider-handle', 'slider-bar', {
+        axis:'horizontal', minimum: 0, maximum: 200, alignX: -5, increment: 2, sliderValue: 0.10
+    });
+
+	demoSlider.options.onSlide = function(value) {
+		scaleIt(value);
+	}
+
+	demoSlider.options.onChange = function(value) {
+		scaleIt(value);
+	}
+
+	scalePhotos = document.getElementsByClassName('scale-image');
+	scaleContainer = document.getElementsByClassName('img-container');
+
+	scaleIt(demoSlider.options.sliderValue);
+
+};
+
+
+
+
