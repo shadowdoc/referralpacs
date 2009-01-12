65d912e6f4462ecaf2a2fcba81cce96b06e76d1e
Marc Kohli <marckohli@gmail.com>
2009-01-12 17:13:12 +0000

    Fixed bug in migration for creating locations.
    git-svn-id: http://referralpacs.googlecode.com/svn/trunk@166 927d8226-84e8-11dd-ad27-0f66e57d2035

--- db/migrate/028_create_locations.rb
+++ db/migrate/028_create_locations.rb
@@ -4,9 +4,14 @@ class CreateLocations < ActiveRecord::Migration
       t.column :name, :string
       t.timestamps
     end
+    
+    add_column :encounter, :location_id, :integer
+    
   end
 
   def self.down
     drop_table :locations
+    
+    remove_column :encounter, :location_id
   end
 end
