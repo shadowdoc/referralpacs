881606ae4774176646cf7658dedc251f031f46a2
shadowdoc <marckohli@gmail.com>
2010-07-23 16:04:51 +0300

    Final version of the monthly statistics mailer

--- app/models/statistics_mailer.rb
+++ app/models/statistics_mailer.rb
@@ -1,7 +1,7 @@
 class StatisticsMailer < ActionMailer::Base
-  def monthly(patients, new, ready_for_printing, radiologist_to_read, final, archived, rejected, ordered, sent_at = Time.now)
+  def monthly(begin_time, end_time, active_providers, patients, new, ready_for_printing, radiologist_to_read, final, archived, rejected, ordered, sent_at = Time.now)
     subject	"ReferralPACS Statistics"
-    body	:patients => patients, :new => new, :ready_for_printing => ready_for_printing, :final => final, :archived => archived, :rejected => rejected, :ordered => ordered
+    body	:begin_time => begin_time, :end_time => end_time, :active_providers => active_providers, :patients => patients, :new => new, :ready_for_printing => ready_for_printing, :final => final, :archived => archived, :rejected => rejected, :ordered => ordered
 #    recipients	["mkohli@iupui.edu", "abuyajm@yahoo.com", "matjohns@iupui.edu", "wanenegl@yahoo.com", "kelvin.ogot@gmail.com"]
     recipients ["mkohli@iupui.edu"]
     from	["mkohli@iupui.edu"]
--- send_monthly_email.rb
+++ send_monthly_email.rb
@@ -1,4 +1,7 @@
-patients = Patient.find(:all).length
+begin_time = Time.now.midnight - 1.month
+end_time = Time.now.midnight
+
+patients = Patient.count
 new = Encounter.find_all_by_status("new").length
 ready_for_printing = Encounter.find_all_by_status("ready_for_printing").length
 radiologist_to_read = Encounter.find_all_by_status("radiologist_to_read").length
@@ -7,4 +10,21 @@ archived = Encounter.find_all_by_status("archived").length
 rejected = Encounter.find_all_by_status("rejected").length
 ordered = Encounter.find_all_by_status("ordered").length
 
-StatisticsMailer.deliver_monthly(patients, new, ready_for_printing, radiologist_to_read, final, archived, rejected, ordered)
+monthly_encounters = Encounter.all(:conditions => {:updated_at => begin_time..end_time}, :include => :provider)
+
+active_providers = Hash.new
+
+monthly_encounters.each do |enc|
+  
+  # Let's create an hash of providers who have read exams over the last month
+
+  if enc.status == "final" || enc.status == "ready_for_printing"
+    unless active_providers.key?(enc.provider.full_name)
+      active_providers[enc.provider.full_name] = 1
+    else
+      active_providers[enc.provider.full_name] += 1
+    end
+  end
+end
+
+StatisticsMailer.deliver_monthly(begin_time, end_time, active_providers, patients, new, ready_for_printing, radiologist_to_read, final, archived, rejected, ordered)
