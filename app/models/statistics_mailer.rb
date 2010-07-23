cc4e924fecbe6b68dc6d0ca8e5feb4b319f30552
shadowdoc <marckohli@gmail.com>
2010-07-23 16:12:40 +0300

    bug fix for statistics mailer

--- app/models/statistics_mailer.rb
+++ app/models/statistics_mailer.rb
@@ -1,9 +1,8 @@
 class StatisticsMailer < ActionMailer::Base
-  def monthly(begin_time, end_time, active_providers, patients, new, ready_for_printing, radiologist_to_read, final, archived, rejected, ordered, sent_at = Time.now)
+  def monthly(begin_time, end_time, local_recipients, active_providers, patients, new, ready_for_printing, radiologist_to_read, final, archived, rejected, ordered, sent_at = Time.now)
     subject	"ReferralPACS Statistics"
     body	:begin_time => begin_time, :end_time => end_time, :active_providers => active_providers, :patients => patients, :new => new, :ready_for_printing => ready_for_printing, :final => final, :archived => archived, :rejected => rejected, :ordered => ordered
-#    recipients	["mkohli@iupui.edu", "abuyajm@yahoo.com", "matjohns@iupui.edu", "wanenegl@yahoo.com", "kelvin.ogot@gmail.com"]
-    recipients ["mkohli@iupui.edu"]
+    recipients local_recipients
     from	["mkohli@iupui.edu"]
     sent_on	sent_at
   end
