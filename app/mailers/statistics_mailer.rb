831b3085236f86ce7f31d3ccce61f68a9588bf6b
Marc Kohli <marckohli@gmail.com>
2013-09-16 13:58:34 -0400

    Fixes and upgrades for StatisticsMailer - using awesome new ActiveRecord syntax

---
+++ app/mailers/statistics_mailer.rb
@@ -0,0 +1,24 @@
+class StatisticsMailer < ActionMailer::Base
+  default :from => "mkohli@iu.edu"
+
+  def monthly
+    # This mailing defaults to 1 month of activity.
+
+    recipients = STATISTICS_EMAIL_LIST
+
+    @begin_time = Time.now.midnight - 1.month
+    @end_time = Time.now.midnight
+
+    @patients = Patient.count
+
+    @stat_hash = Encounter.group(:status).count
+
+    @new = Encounter.where(status: "new").count
+
+    @active_providers = Encounter.includes(:provider).group(:provider).where(:updated_at => @begin_time..@end_time).count
+
+    mail(:subject => "ReferralPACS Statistics",
+    	 :date => Time.now,
+    	 :to => STATISTICS_EMAIL_LIST)
+  end
+end
--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -1,11 +1,12 @@
 # Load mail configuration if not in test environment
-if (Rails.env.development? || Rails.env.production?)  && File.exists?(Rails.root.join("/config/email.yml"))
+if (Rails.env.development? || Rails.env.production?)  && File.exists?(Rails.root.join("config/email.yml"))
+  
   # This loads configuration settings for the monthly statistics e-mails
   # All of the settings except for recipients are the basic smtp settings
   # We assign the recipeints to a global constant and then remove it from the
   # hash that we pass to ActionMailer
 
-  email_settings = YAML::load(File.open(Rails.root.join("/config/email.yml")))
+  email_settings = YAML::load(File.open(Rails.root.join("config/email.yml")))
 
   STATISTICS_EMAIL_LIST = email_settings[Rails.env].delete(:recipients)
 
