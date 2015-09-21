36fbab0774870221458c0bdad87ee6f06565b085
Marc Kohli (shadowdoc) <marckohli@gmail.com>
2015-09-21 11:13:49 +0300

    start DICOM listener when rails boots config from dcm4chee.yml

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Ref::Application.initialize!
+
+
+if DICOM_LOCAL_PORT
+	s = DICOM::DServer.new(DICOM_LOCAL_PORT, :host_ae => DICOM_LOCAL_AET)
+	t = Thread.new { s.start_scp("./public/dicom/") }
+end
--- config/initializers/dcm4chee.rb
+++ config/initializers/dcm4chee.rb
@@ -1,14 +1,20 @@
 # Load DCM4CHEE settings from file config/dcm4chee.yml
 # Also remember to add the pacsdb settings to config/database.yml
 
-if (Rails.env.development? || Rails.env.production?) && File.exists?(Rails.root.join("config/dcm4chee.yml"))
+if (Rails.env.development? || Rails.env.production? || Rails.env.staging?) && File.exists?(Rails.root.join("config/dcm4chee.yml"))
 
   settings = YAML::load(File.open(Rails.root.join("config/dcm4chee.yml")))
 
   settings = settings[Rails.env]
   DCM4CHEE_URL_BASE = settings[:url]
+  DCM4CHEE_REMOTE_PORT = settings[:remote_dicom_port]
+  DCM4CHEE_REMOTE_HOST = settings[:remote_dicom_host]
+  DCM4CHEE_REMOTE_AET = settings[:remote_dicom_aet]
+
+  DICOM_LOCAL_PORT = settings[:local_dicom_port]
+  DICOM_LOCAL_AET = settings[:local_dicom_aet]
 
 else
-  # set OPENMRS_SETTINGS to nil
   DCM4CHEE_URL_BASE = nil
+  DICOM_LOCAL_PORT = nil
 end
