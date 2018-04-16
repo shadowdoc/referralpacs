77512f2f77bfa60b323c14fa7564e20417266378
Marc Kohli (shadowdoc) <marckohli@gmail.com>
2018-04-16 11:52:51 -0700

    updates to fhir_controller to support date searching

--- app/controllers/fhir_controller.rb
+++ app/controllers/fhir_controller.rb
@@ -20,16 +20,35 @@ class FhirController < ApplicationController
 	end
 
 	def diagnosticreport
-		Rails.logger.info("fhir - search request for openmrs_mrn: #{params[:patient]} - user: #{@api_user.email}")
 
-		if params[:patient].nil? && params[:id].nil?
+		# FHIR diagnostic report interface with limited search support (https://www.hl7.org/fhir/search.html)
+		# currently supported:
+		# - query with a single Encounter ID
+		# - patient searches
+		# - date searches with the following prefixes - eq, ge, le, gt, lt  (https://www.hl7.org/fhir/search.html#prefix)
+
+		Rails.logger.info("fhir - search request params: #{params} - user: #{@api_user.email}")
+
+		# We do not allow open queries
+		if params[:patient].nil? && params[:id].nil? && params[:date].nil?
 			# code to return a failure
 			render :error_open_query and return
 		end
 
-		if params[:patient]
+		# We have both patient and date searching
+		if params[:patient] && params[:date]
 			patient = Patient.includes(:encounters).find_openmrs(params[:patient])
-	
+
+			operator = get_date_operator(params[:date])
+
+			@encounters = patient.encounters.where("date " + operator + " ?", Date.parse(params[:date]))
+
+		end
+
+		# Searching with patient identifier (OpenMRS ID)
+		if params[:patient] && @encounters.nil?
+			patient = Patient.includes(:encounters).find_openmrs(params[:patient])
+
 			if patient.nil?
 				# No patient returns an empty bundle
 				@encounters = []
@@ -37,12 +56,21 @@ class FhirController < ApplicationController
 			else
 				@encounters = patient.encounters
 			end
-		else 
+		end
+
+		if params[:date] && @encounters.nil?
+			operator = get_date_operator(params[:date])
+			@encounters = Encounter.where("date " + operator + " ?", Date.parse(params[:date]))
+		end
+
+
+		if params[:id]
 			@encounters = [Encounter.find(params[:id])]
 		end
 
 		# Return a bundle (no or multiple results), or a resource (single result)
-		if @encounters.nil?
+
+		if @encounters.nil? || @encounters.length == 0
 			@encounters = []
 			render :bundle and return
 		end
@@ -68,4 +96,19 @@ class FhirController < ApplicationController
 		end
 	end
 
+
+	def get_date_operator(date_parameter)
+		case date_parameter[0..1]
+			when "gt"
+				operator = ">"
+			when "lt"
+				operator = "<"
+			when "ge"
+				operator = ">="
+			when "le"
+				operator = "<="
+			else
+				operator = "="
+		end
+	end
 end
